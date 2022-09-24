63f9151c977c85201cbf3972bb9e51fc653bc319
evan-mick <53022037+evan-mick@users.noreply.github.com>
2022-09-24 12:49:15 -0700

    FSM Start

--- Assets/Scripts/AI/FiniteStateMachine.cs
+++ Assets/Scripts/AI/FiniteStateMachine.cs
@@ -6,9 +6,50 @@ public class FiniteStateMachine : MonoBehaviour
 {
     private FSMState[] states;
     private FSMState currentState;
-    private FSMState startingState; 
+    private FSMState startingState;
 
-    
+    [SerializeField]
+    private AIBlackboard blackboard;
+
+
+    public List<FSMAction> UpdateStateMachine()
+    {
+        List<FSMAction> returnActions = new List<FSMAction>();
+
+        FSMTransition triggered = null;
+
+        
+
+
+        foreach (FSMTransition transition in currentState.Transitions)
+        {
+            if (transition.IsTriggered(blackboard))
+            {
+                triggered = transition;
+                break; 
+            }
+        }
+
+
+        // Transition to a new state!
+        if (triggered != null)
+        {
+            FSMState target = triggered.TargetState;
+
+            returnActions.AddRange(currentState.ExitActions);
+            returnActions.AddRange(triggered.Actions);
+            returnActions.AddRange(target.EntryActions);
+
+            currentState = target;
+            return returnActions;
+        }
+        
+        
+        return currentState.Actions;
+
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -28,18 +69,45 @@ public class HierarchicalStateMachine : MonoBehaviour
 
 }
 
+public class FSMAction
+{
+
+}
+
 
 
 public class FSMState
 {
-    private Dictionary<FSMState, FSMTransition> transitions = new Dictionary<FSMState, FSMTransition>();
+    //private Dictionary<FSMState, FSMTransition> transitions = new Dictionary<FSMState, FSMTransition>();
 
+
+    protected List<FSMTransition> transitions = new List<FSMTransition>();
+    public ref List<FSMTransition> Transitions { get { return ref transitions; } }
+
+
+    protected List<FSMAction> actions = new List<FSMAction>(); 
+    public ref List<FSMAction> Actions { get { return ref actions; } }
+
+
+    protected List<FSMAction> entryActions = new List<FSMAction>();
+    public ref List<FSMAction> EntryActions { get { return ref entryActions; } }
+
+
+    protected List<FSMAction> exitActions = new List<FSMAction>();
+    public ref List<FSMAction> ExitActions { get { return ref exitActions; } }
 
 
 }
 
-public class FSMTransition
+public abstract class FSMTransition
 {
+    protected FSMState targetState;
+    public ref FSMState TargetState { get { return ref targetState;  } }
 
+
+    protected List<FSMAction> actions = new List<FSMAction>();
+    public ref List<FSMAction> Actions { get { return ref actions; } }
+
+    public abstract bool IsTriggered(AIBlackboard blackboard);
 }
 
