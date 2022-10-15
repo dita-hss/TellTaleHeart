80b3b048bda3fe9f8d342b4637e20855bfff3d9a
Amanda Hernandez Sandate <amanda_hernandez_sandate@brown.edu>
2022-10-15 11:15:27 -0400

    Door and Key

---
+++ Assets/Scripts/DoorAndKey/KeyInventory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// keeps everything related to this sytem in itself
+namespace KeySystem
+{
+    public class KeyInventory : MonoBehaviour
+    {
+
+        public bool hasRedKey = false;
+
+
+    }
+}
